94638edbd5b53454fe696553336fc0d8d1d4cdd7
Ben Marini <bmarini@gmail.com>
2010-03-27 10:23:11 -0700

    Installing configs to etc/cassandra, fixing paths in config files

--- Library/Formula/cassandra.rb
+++ Library/Formula/cassandra.rb
@@ -8,21 +8,23 @@ class Cassandra <Formula
   def install
     (var+"lib/cassandra").mkpath
     (var+"log/cassandra").mkpath
+    (etc+"cassandra").mkpath
 
-    inreplace "conf/storage-conf.xml" do |s|
-      s.gsub! "/var/lib/cassandra", "#{var}/lib/cassandra"
-    end
-
-    inreplace "conf/log4j.properties" do |s|
-      s.gsub! "/var/log/cassandra", "#{var}/log/cassandra"
-    end
+    inreplace "conf/storage-conf.xml", "/var/lib/cassandra", "#{var}/lib/cassandra"
+    inreplace "conf/log4j.properties", "/var/log/cassandra", "#{var}/log/cassandra"
 
     inreplace "bin/cassandra.in.sh" do |s|
       s.gsub! "cassandra_home=`dirname $0`/..", "cassandra_home=#{prefix}"
+      # Store configs in etc, outside of keg
+      s.gsub! "CASSANDRA_CONF=$cassandra_home/conf", "CASSANDRA_CONF=#{etc}/cassandra"
+      # Jars installed to prefix, no longer in a lib folder
+      s.gsub! "$cassandra_home/lib/*.jar", "$cassandra_home/*.jar"
     end
 
     rm Dir["bin/*.bat"]
-    prefix.install Dir["*.txt"] + Dir["{bin,conf,interface,javadoc}"]
+
+    (etc+"cassandra").install Dir["conf/*"]
+    prefix.install Dir["*.txt"] + Dir["{bin,interface,javadoc}"]
     prefix.install Dir["lib/*.jar"]
   end
 end
